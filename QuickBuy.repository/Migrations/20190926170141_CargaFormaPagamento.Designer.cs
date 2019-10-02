905f0efc66e511626b68459c5d63ff9d1ba76fc1
Ícaro Miranda <miranda.icarox@gmail.com>
2019-10-02 13:24:52 -0300

    02/10/2019

--- QuickBuy.repository/Migrations/20190926170141_CargaFormaPagamento.Designer.cs
+++ QuickBuy.repository/Migrations/20190926170141_CargaFormaPagamento.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using QuickBuy.repository.Contexto;
 
 namespace QuickBuy.repository.Migrations
 {
     [DbContext(typeof(QuickBuyContexto))]
-    partial class QuickBuyContextoModelSnapshot : ModelSnapshot
+    [Migration("20190926170141_CargaFormaPagamento")]
+    partial class CargaFormaPagamento
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
