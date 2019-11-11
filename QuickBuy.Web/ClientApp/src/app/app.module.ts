770b574f79ae2ddda80c80a6f407f37d0fb9b949
Ícaro Miranda <miranda.icarox@gmail.com>
2019-11-11 18:29:02 -0300

    Limpando arquivos do template padrão o Vs2019

--- QuickBuy.Web/ClientApp/src/app/app.module.ts
+++ QuickBuy.Web/ClientApp/src/app/app.module.ts
@@ -7,8 +7,6 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './nav-menu/nav-menu.component';
 import { HomeComponent } from './home/home.component';
-import { CounterComponent } from './counter/counter.component';
-import { FetchDataComponent } from './fetch-data/fetch-data.component';
 import { ProdutoComponent } from './produto/produto.component';
 import { LoginComponent } from './usuario/login/login.component';
 import { GuardaRotas } from './autorizacao/guarda.rotas';
@@ -22,8 +20,6 @@ import { ProdutoServico } from './servicos/produto/produto.servico';
     AppComponent,
     NavMenuComponent,
     HomeComponent,
-    CounterComponent,
-    FetchDataComponent,
     ProdutoComponent,
     LoginComponent,
     CadastroProdutoComponent,
@@ -35,8 +31,6 @@ import { ProdutoServico } from './servicos/produto/produto.servico';
     FormsModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full' },
-      { path: 'counter', component: CounterComponent },
-      { path: 'fetch-data', component: FetchDataComponent },
       { path: 'produto', component: ProdutoComponent, canActivate: [GuardaRotas] },
       { path: 'novo-produto', component: CadastroProdutoComponent, canActivate: [GuardaRotas] },
       { path: 'entrar', component: LoginComponent },
